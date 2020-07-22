ce097ad9bb64f67023d2ee5a397d02f381a5b7fc
cq-mohtashim-ali <muhtashim.alikhan@gmail.com>
2020-07-22 22:28:22 +0500

    3rd stable Commit

--- src/app.js
+++ src/app.js
@@ -1,18 +1,11 @@
 const fs = require("fs");
-// const { json } = require('body-parser');
 const WebSocket = require("ws").Server;
 const mime = require("mime");
+const { decodeBase64 } = require("bcryptjs");
+const path = require("path");
 
 const wss = new WebSocket({ port: 8080 });
 const UserArray = [{ name: "Ali" }, { name: "Ahmad" }, { name: "Asad" }];
-const currentDate = new Date();
-const day = `${currentDate.getMonth() + 1 < 10 ? "0" : ""}${currentDate.getMonth() + 1}`;
-const month = `${currentDate.getDate() < 10 ? "0" : ""}${currentDate.getDate()}`;
-const year = currentDate.getFullYear();
-const hours = currentDate.getHours();
-const minutes = currentDate.getMinutes();
-const seconds = currentDate.getSeconds();
-const timeStamp = currentDate.getTime();
 
 function checkUser(name) {
   const value = UserArray.find((val) => val.name === name);
@@ -22,9 +15,6 @@ function checkUser(name) {
 
   return 1;
 }
-// console.log(`${year}-${month}-${day}-${hours}${minutes}-${seconds}-${timeStamp}`);
-// let number = 10;
-const ImgaesDir = `./images/${year}/${month}/${day}`;
 wss.on("connection", (ws) => {
   let number;
   ws.on("message", (message) => {
@@ -33,20 +23,33 @@ wss.on("connection", (ws) => {
 
     const numbr = checkUser(data.name);
     console.log(numbr);
-    // timeStamp = 100000;
-    // console.log(timeStamp);
     if (numbr === 1) {
-      ws.send("2000");
+      ws.send("8000");
     } else {
       ws.send("404");
     }
-    // let imageBuffer = new Buffer(data.image, "base64");
-    const fileName = `${year}-${month}-${day}-${hours}${minutes}-${seconds}-${timeStamp}`;
-    try {
-      fs.mkdirSync(ImgaesDir, { recursive: true });
-      // fs.writeFileSync("./images" + fileName, imageBuffer, 'utf8');
-    } catch (e) {
-      console.log(e);
-    }
+    // if (data.image != null) {
+    //   try {
+
+      const currentDate = new Date();
+      const day = `${currentDate.getMonth() + 1 < 10 ? "0" : ""}${currentDate.getMonth() + 1}`;
+      const month = `${currentDate.getDate() < 10 ? "0" : ""}${currentDate.getDate()}`;
+      const year = currentDate.getFullYear();
+      const hours = currentDate.getHours();
+      const minutes = currentDate.getMinutes();
+      const seconds = currentDate.getSeconds();
+      const timeStamp = currentDate.getTime();
+    const images = Buffer.from(data.image, "base64");
+    const ImgaesDir = `images/${data.name}/${year}/${month}/${day}/`;
+    const fileName = `${data.name}-${year}-${month}-${day}-${hours}${minutes}-${seconds}-${timeStamp}.png`;
+    const p = path.join(__dirname, `../images/${data.name}/${year}/${month}/${day}/${fileName}`);
+    fs.mkdirSync(ImgaesDir, { recursive: true });
+    fs.writeFileSync(p, images, "utf8");
+
+    //   } catch (e) {
+    //     console.log(e);
+    //   }
+    // }
   });
 });
+// console.log(ImgaesDir)
