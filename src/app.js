7bca4e5f1e4e4ed56f43194fe7d388f26e51f132
cq-mohtashim-ali <muhtashim.alikhan@gmail.com>
2020-07-24 21:07:07 +0500

    7th stable commit

--- src/app.js
+++ src/app.js
@@ -1,22 +1,33 @@
 const WebSocket = require("ws").Server;
+const opn = require("opn");
+const path = require("path");
 const check = require("./components/checkUser");
-const saveImg = require("./components/saveImage")
+const saveImg = require("./components/saveImage");
 
+const wssClient = new WebSocket({ port: 9000 });
 const wss = new WebSocket({ port: 8080 });
 wss.on("connection", (ws) => {
-  let number;
+  opn(path.join(__dirname, "public", "index.html"));
   ws.on("message", (message) => {
     const data = JSON.parse(message);
     console.log(data.name);
     const numbr = check.checkUser(data.name);
     console.log(numbr);
     if (numbr === 1) {
-      setTimeout(()=>{
-        ws.send("Send Images");
-      },10000)
+      // setTimeout(() => {
+      ws.send("Send Images");
+      // }, 5000);
     } else {
       ws.send("404");
     }
-saveImg.saveImage(data.image,data.name)
+    if (data.image !== "image") {
+      wssClient.on("connection", (websocket) => {
+        websocket.send(data.image);
+      });
+    }
+
+    saveImg.saveImage(data.image, data.name);
   });
 });
+
+module.exports.client = wssClient;
--- src/components/saveImage.js
+++ src/components/saveImage.js
@@ -1,21 +1,25 @@
+const WebSocket = require("ws").Server;
+const path = require("path");
+const fs = require("fs");
 const getTimedNames = require("./date");
-const path = require("path")
-const fs = require("fs")
+const wssClient = require("../app");
 
-function saveImage(image,name){
-    if (image !== "image") {
-        const images = Buffer.from(image, "base64");
-        const timedNames = getTimedNames.getTimedName(name);
-        const p = path.join(__dirname, timedNames[1]);
-        fs.mkdir(timedNames[0], { recursive: true }, (err) => {
-          if (err) throw err;
-          fs.writeFile(p, images, () => {
-            console.log("File saved!");
-          });
-        });
-      }
-      else{
-          console.log("Image Not Received")
-      }
-} 
+function saveImage(image, name) {
+  if (image !== "image") {
+    wssClient.client.on("connection", (websocket) => {
+      websocket.send(image);
+    });
+    const images = Buffer.from(image, "base64");
+    const timedNames = getTimedNames.getTimedName(name);
+    const p = path.join(__dirname, timedNames[1]);
+    fs.mkdir(timedNames[0], { recursive: true }, (err) => {
+      if (err) throw err;
+      fs.writeFile(p, images, () => {
+        console.log("File saved!");
+      });
+    });
+  } else {
+    console.log("Image Not Received");
+  }
+}
 module.exports.saveImage = saveImage;
