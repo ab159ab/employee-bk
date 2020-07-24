838c2cce6ec47383e25d6b616b0b56a7b3d8a7a3
cq-mohtashim-ali <muhtashim.alikhan@gmail.com>
2020-07-24 21:18:33 +0500

    Separate Functional Units

--- src/app.js
+++ src/app.js
@@ -1,16 +1,12 @@
-const fs = require("fs");
 const WebSocket = require("ws").Server;
-const mime = require("mime");
-const path = require("path");
 const opn = require("opn");
-const getTimedNames = require("./components/date");
+const path = require("path");
 const check = require("./components/checkUser");
+const saveImg = require("./components/saveImage");
 
 const wssClient = new WebSocket({ port: 9000 });
 const wss = new WebSocket({ port: 8080 });
 wss.on("connection", (ws) => {
-  let number;
-  // opn(`${__dirname}\\public\\index.html`);
   opn(path.join(__dirname, "public", "index.html"));
   ws.on("message", (message) => {
     const data = JSON.parse(message);
@@ -21,24 +17,14 @@ wss.on("connection", (ws) => {
       setTimeout(() => {
         ws.send("Send Images");
       }, 5000);
-
-      if (data.image !== "image") {
-        wssClient.on("connection", (websocket) => {
-          websocket.send(data.image);
-        });
-        const images = Buffer.from(data.image, "base64");
-        const timedNames = getTimedNames.getTimedName(data.name);
-        const p = path.join(__dirname, timedNames[1]);
-        fs.mkdir(timedNames[0], { recursive: true }, (err) => {
-          if (err) throw err;
-          fs.writeFile(p, images, (error) => {
-            if (err) throw err;
-            console.log("File saved!");
-          });
-        });
-      }
     } else {
       ws.send("404");
     }
+    if (data.image !== "image") {
+      wssClient.on("connection", (websocket) => {
+        websocket.send(data.image);
+      });
+    }
+    saveImg.saveImage(data.image, data.name);
   });
 });
--- src/components/saveImage.js
+++ src/components/saveImage.js
@@ -1,14 +1,10 @@
-const WebSocket = require("ws").Server;
+
 const path = require("path");
 const fs = require("fs");
 const getTimedNames = require("./date");
-const wssClient = require("../app");
 
 function saveImage(image, name) {
   if (image !== "image") {
-    wssClient.client.on("connection", (websocket) => {
-      websocket.send(image);
-    });
     const images = Buffer.from(image, "base64");
     const timedNames = getTimedNames.getTimedName(name);
     const p = path.join(__dirname, timedNames[1]);
