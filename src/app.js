cc2a9d36fd8959aadc71b2792b47eeda06bda119
Osaf <Osaf96@gmail.com>
2020-07-24 23:21:00 +0500

    Live Screenshots Completed

--- src/app.js
+++ src/app.js
@@ -1,26 +1,21 @@
 const WebSocket = require("ws").Server;
 const opn = require("opn");
-<<<<<<< HEAD
 const { json } = require("body-parser");
-const userLogs = require("./components/userLogs");
-=======
-const path = require("path");
->>>>>>> 28095b6edb103597798e7384395e18000b0e43ee
+const { Socket } = require("dgram");
 const getTimedNames = require("./components/date");
 const check = require("./components/checkUser");
 const saveImg = require("./components/saveImage");
 
 const wssClient = new WebSocket({ port: 9000 });
+wssClient.on("connection", (websocket) => {
+  console.log("connection opened on port 9000");
+});
 const wss = new WebSocket({ port: 8080 });
 wss.on("connection", (ws) => {
-<<<<<<< HEAD
   let number;
   let userName;
-  userLogs.logInTime(new Date());
-  // opn(`${__dirname}\\public\\index.html`);
-=======
-  opn(path.join(__dirname, "public", "index.html"));
->>>>>>> 28095b6edb103597798e7384395e18000b0e43ee
+  let isClosed = false;
+  opn(`${__dirname}\\public\\index.html`);
   ws.on("message", (message) => {
     const data = JSON.parse(message);
     console.log(data.name);
@@ -28,44 +23,23 @@ wss.on("connection", (ws) => {
     const numbr = check.checkUser(data.name);
     console.log(numbr);
     if (numbr === 1) {
-<<<<<<< HEAD
-      ws.send("8000");
-      if (data.image !== "image") {
-        const currentDate = new Date();
-        wssClient.on("connection", (websocket) => {
-          const obj = { date: currentDate, image: data.image };
-          websocket.send(JSON.stringify(obj));
-        });
-        const images = Buffer.from(data.image, "base64");
-        const timedNames = getTimedNames.getTimedName(data.name);
-        const p = path.join(__dirname, timedNames[1]);
-        // fs.mkdirSync(timedNames[0], { recursive: true });
-        // fs.writeFileSync(p, images, "utf8");
-        fs.mkdir(timedNames[0], { recursive: true }, (err) => {
-          if (err) throw err;
-          fs.writeFile(p, images, (error) => {
-            if (err) throw err;
-            console.log("File saved!");
-          });
-        });
-      }
-=======
       setTimeout(() => {
         ws.send("Send Images");
       }, 5000);
->>>>>>> 28095b6edb103597798e7384395e18000b0e43ee
     } else {
       ws.send("404");
     }
     if (data.image !== "image") {
-      wssClient.on("connection", (websocket) => {
-        websocket.send(data.image);
+      wssClient.clients.forEach((client) => {
+        const obj = { date: new Date(), image: data.image };
+        client.send(JSON.stringify(obj));
+        console.log(client);
       });
     }
     saveImg.saveImage(data.image, data.name);
   });
   ws.on("close", () => {
-    userLogs.logOutTime(new Date(), userName);
+    isClosed = true;
     console.log(`[Server]: Connection is Closed with client: ${userName}`);
   });
 });
--- src/components/userLogs.js
+++ src/components/userLogs.js
@@ -5,7 +5,7 @@ const logTime = {
   logInDate: null,
   logOutState: null,
   logoutDate: null,
-  sessionTime:null,
+  sessionTime: null,
 };
 function logInTime(Date) {
   logTime.loginState = "Logged In";
@@ -15,14 +15,17 @@ function logInTime(Date) {
 function logOutTime(Date, userName) {
   const logObj = timeArray[0];
   logObj.username = userName;
-  logObj.logOutState = "Logged Out"
+  logObj.logOutState = "Logged Out";
   logObj.logoutDate = Date;
 
-  //calculate session time
+  // calculate session time
   console.log(logObj);
   // Save this object to postgre database
 }
 
+// const userLogs = require("./components/userLogs");
+// userLogs.logInTime(new Date());
+// userLogs.logOutTime(new Date(), userName);
 module.exports = {
   logInTime,
   logOutTime,
