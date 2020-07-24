b85168e05c8c4ab96de235a54b548c119942cced
Osaf <Osaf96@gmail.com>
2020-07-24 22:29:46 +0500

    working on user session time

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const WebSocket = require("ws").Server;
 const mime = require("mime");
 const path = require("path");
 const opn = require("opn");
+const { json } = require("body-parser");
+const userLogs = require("./components/userLogs");
 const getTimedNames = require("./components/date");
 const check = require("./components/checkUser");
 
@@ -10,17 +12,22 @@ const wssClient = new WebSocket({ port: 9000 });
 const wss = new WebSocket({ port: 8080 });
 wss.on("connection", (ws) => {
   let number;
-  opn(`${__dirname}\\public\\index.html`);
+  let userName;
+  userLogs.logInTime(new Date());
+  // opn(`${__dirname}\\public\\index.html`);
   ws.on("message", (message) => {
     const data = JSON.parse(message);
     console.log(data.name);
+    userName = data.name;
     const numbr = check.checkUser(data.name);
     console.log(numbr);
     if (numbr === 1) {
       ws.send("8000");
       if (data.image !== "image") {
+        const currentDate = new Date();
         wssClient.on("connection", (websocket) => {
-          websocket.send(data.image);
+          const obj = { date: currentDate, image: data.image };
+          websocket.send(JSON.stringify(obj));
         });
         const images = Buffer.from(data.image, "base64");
         const timedNames = getTimedNames.getTimedName(data.name);
@@ -38,5 +45,9 @@ wss.on("connection", (ws) => {
     } else {
       ws.send("404");
     }
+  });
+  ws.on("close", () => {
+    userLogs.logOutTime(new Date(), userName);
+    console.log(`[Server]: Connection is Closed with client: ${userName}`);
   });
 });
---
+++ src/components/userLogs.js
@@ -0,0 +1,29 @@
+const timeArray = [];
+const logTime = {
+  loginState: null,
+  username: null,
+  logInDate: null,
+  logOutState: null,
+  logoutDate: null,
+  sessionTime:null,
+};
+function logInTime(Date) {
+  logTime.loginState = "Logged In";
+  logTime.logInDate = Date;
+  timeArray.push(logTime);
+}
+function logOutTime(Date, userName) {
+  const logObj = timeArray[0];
+  logObj.username = userName;
+  logObj.logOutState = "Logged Out"
+  logObj.logoutDate = Date;
+
+  //calculate session time
+  console.log(logObj);
+  // Save this object to postgre database
+}
+
+module.exports = {
+  logInTime,
+  logOutTime,
+};
