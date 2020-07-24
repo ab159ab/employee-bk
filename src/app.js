e3ce3b71f849d315490aa6d589c79ce77524b4bf
cq-mohtashim-ali <muhtashim.alikhan@gmail.com>
2020-07-24 21:11:38 +0500

    7th stable commit

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
+const fs = require("fs");
 const WebSocket = require("ws").Server;
-const opn = require("opn");
+const mime = require("mime");
 const path = require("path");
+const opn = require("opn");
+const getTimedNames = require("./components/date");
 const check = require("./components/checkUser");
-const saveImg = require("./components/saveImage");
 
 const wssClient = new WebSocket({ port: 9000 });
 const wss = new WebSocket({ port: 8080 });
 wss.on("connection", (ws) => {
+  let number;
+  // opn(`${__dirname}\\public\\index.html`);
   opn(path.join(__dirname, "public", "index.html"));
   ws.on("message", (message) => {
     const data = JSON.parse(message);
@@ -14,20 +18,27 @@ wss.on("connection", (ws) => {
     const numbr = check.checkUser(data.name);
     console.log(numbr);
     if (numbr === 1) {
-      // setTimeout(() => {
-      ws.send("Send Images");
-      // }, 5000);
+      setTimeout(() => {
+        ws.send("Send Images");
+      }, 5000);
+
+      if (data.image !== "image") {
+        wssClient.on("connection", (websocket) => {
+          websocket.send(data.image);
+        });
+        const images = Buffer.from(data.image, "base64");
+        const timedNames = getTimedNames.getTimedName(data.name);
+        const p = path.join(__dirname, timedNames[1]);
+        fs.mkdir(timedNames[0], { recursive: true }, (err) => {
+          if (err) throw err;
+          fs.writeFile(p, images, (error) => {
+            if (err) throw err;
+            console.log("File saved!");
+          });
+        });
+      }
     } else {
       ws.send("404");
     }
-    if (data.image !== "image") {
-      wssClient.on("connection", (websocket) => {
-        websocket.send(data.image);
-      });
-    }
-
-    saveImg.saveImage(data.image, data.name);
   });
 });
-
-module.exports.client = wssClient;
